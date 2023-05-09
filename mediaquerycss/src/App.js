f7749d81ca1553923b51ef4a69b7e61379f2e0c7
riya singh <riyafriday5@gmail.com>
2023-05-09 10:08:37 +0530

    code push

--- mediaquerycss/src/App.js
+++ mediaquerycss/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Nav from './organisms/Nav';
-
+import Pages from './Pages/Pages';
 
 export default function App() {
   return (
     <div>
      <Nav/>
-  
+    <Pages/>
     </div>
   )
 }
--- mediaquerycss/src/organisms/Nav.js
+++ mediaquerycss/src/organisms/Nav.js
@@ -1,28 +1,85 @@
-import React from "react";
-import './Nav.css'
+import React, { useState } from "react";
+import './Nav.css';
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaBeer } from "react-icons/fa";
 
-export default function Nav() {
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
-    <div>
-      <header className="header">
-        <div className="nav-head">
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+            <span>logo</span>
+            
+          </h2>
+        </div>
+
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
           <ul>
-            <li>Home</li>
-          </ul>
-          <ul>
-            <li>About</li>
-          </ul>
-          <ul>
-            <li>Projcts</li>
-          </ul>
-          <ul>
-            <li>Videos</li>
-          </ul>
-          <ul>
-            <li>Contact</li>
+            <li>
+              <a to="/">Home</a>
+            </li>
+            <li>
+              <a to="/about">about</a>
+            </li>
+            <li>
+              <a to="/service">project</a>
+            </li>
+            <li>
+              <a to="/contact">video</a>
+            </li>
+            <li>
+              <a to="/">contact</a>
+            </li>
+            <button className="btn">
+              login
+            </button>
           </ul>
         </div>
-      </header>
-    </div>
+
+        {/* 3rd social media links */}
+        <div className="social-media">
+          <ul className="social-media-desktop">
+            <li>
+             
+            </li>
+            <li>
+             
+            </li>
+            <li>
+             
+            </li>
+          </ul>
+
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu/>
+            </a>
+          </div>
+        </div>
+      </nav>
+
+      {/* hero section  */}
+      {/* <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Thapa Technical</h1>
+      </section> */}
+    </>
   );
-}
+};
+
+export default Nav;
